9633acf301e0310fe0007f7155282e243ca1e9b4
Igor Maliy <imaliy.filsh@gmail.com>
2021-02-06 11:47:31 +0200

    Development

--- config/params.php
+++ config/params.php
@@ -2,27 +2,11 @@
 
 use Mailery\Channel\Email\EmailChannel;
 use Yiisoft\Factory\Definitions\Reference;
-use Yiisoft\Router\UrlGeneratorInterface;
 
 return [
     'maileryio/mailery-channel' => [
         'channels' => [
             Reference::to(EmailChannel::class),
-        ],
-    ],
-
-    'maileryio/mailery-brand' => [
-        'settings-menu' => [
-            'items' => [
-                'aws-settings' => [
-                    'label' => static function () {
-                        return 'AWS Credentials';
-                    },
-                    'url' => static function (UrlGeneratorInterface $urlGenerator) {
-                        return $urlGenerator->generate('/brand/default/index');
-                    },
-                ],
-            ],
         ],
     ],
 ];
