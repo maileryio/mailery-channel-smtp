6daefa358415de90bed14ba2b563e3e653057aeb
Igor Maliy <imaliy.filsh@gmail.com>
2022-07-08 00:16:10 +0300

    Development

--- config/common.php
+++ config/common.php
@@ -3,7 +3,7 @@
 use Psr\Container\ContainerInterface;
 use Mailery\Channel\Smtp\Model\SmtpChannelType;
 use Mailery\Campaign\Recipient\Model\RecipientIterator;
-use Mailery\Channel\Smtp\Factory\RecipientFactory;
+use Mailery\Campaign\Recipient\Factory\RecipientFactory;
 use Mailery\Channel\Smtp\Factory\IdentificatorFactory;
 use Mailery\Channel\Smtp\Handler\ChannelHandler;
 
--- src/Handler/ChannelHandler.php
+++ src/Handler/ChannelHandler.php
@@ -4,52 +4,17 @@ namespace Mailery\Channel\Smtp\Handler;
 
 use Mailery\Campaign\Entity\Sendout;
 use Mailery\Campaign\Entity\Recipient;
-use Mailery\Channel\Smtp\Factory\MailerFactory;
 use Mailery\Channel\Handler\HandlerInterface;
-use Cycle\ORM\ORMInterface;
-use Yiisoft\Yii\Cycle\Data\Writer\EntityWriter;
-use Mailery\Sender\Email\Entity\EmailSender;
-use Mailery\Template\Email\Entity\EmailTemplate;
 
 class ChannelHandler implements HandlerInterface
 {
-
-    /**
-     * @param ORMInterface $orm
-     * @param MailerFactory $mailerFactory
-     */
-    public function __construct(
-        private ORMInterface $orm,
-        private MailerFactory $mailerFactory
-    ) {}
 
     /**
      * @inheritdoc
      */
     public function handle(Sendout $sendout, Recipient $recipient): bool
     {
-        $recipient->setSendout($sendout);
-        (new EntityWriter($this->orm))->write([$recipient]);
-
-        $mailer = $this->mailerFactory->create();
-
-        /** @var EmailSender $sender */
-        $sender = $sendout->getCampaign()->getSender();
-        /** @var EmailTemplate $template */
-        $template = $sendout->getCampaign()->getTemplate();
-
-        $message = $this->mailer->compose()
-            ->withTo([$recipient->getIdentifier() => $recipient->getName()])
-            ->withFrom([$sender->getEmail() => $sender->getName()])
-            ->withReplyTo([$sender->getReplyEmail() => $sender->getReplyName()])
-            ->withSubject($sendout->getCampaign()->getName())
-            ->withTextBody($template->getTextContent())
-            ->withHtmlBody($template->getHtmlContent())
-        ;
-
-        $this->mailer->send($message);
-
-        return true;
+        throw new \Exception('Not implemented');
     }
 
 }
